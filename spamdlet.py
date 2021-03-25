6adf89d5cf0b135c20e298c98f07ee76529b62f5
Kodzima <76056308+Kodzima@users.noreply.github.com>
2021-03-25 22:19:50 +0000

    Update spamdlet.py

--- spamdlet.py
+++ spamdlet.py
@@ -3,6 +3,7 @@ import threading
 import os
 import time
 import json
+from bs4 import BeautifulSoup as bs4
 
 
 os.system("clear")
@@ -23,12 +24,21 @@ threads = 1
 
 search = input("Введите ваш запрос для фото: ").replace(" ", "+")
 
-wall_id = int(input('Введите wall_id: '))
-wall_section_id = int(input('Введите wall_section_id: '))
+
+url = input("Введите ссылку на нужный вам падлет: ")
 subject = input('Введите заголовок для вложения (по желанию): ')
 body = input('Введите текст для вложения (по желанию): ')
 threads = int(input('Введите количество потоков для спамма (по умолчанию 1): '))
 
+response = rq.get(url)
+soup = bs4(response.content, 'lxml')
+startingStatePreload = soup.find('link', {'id' : 'starting-state-preload'}).get('href')
+response = rq.get('https://padlet.com' + startingStatePreload)
+response = json.loads(response.content)
+wall_id = response['wall']['id']
+response = rq.get('https://padlet.com/api/5/wall_sections?wall_id=' + str(wall_id))
+response = json.loads(response.content)
+wall_section_id = response['data'][0]['id']
 headers = {
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
     'authorization' : 'Bearer 63002bc7cc05f2a3157ad7945e958e22cde27e81358c16ec53c7082b151a8010'
@@ -47,7 +57,7 @@ def main():
                             'attachment' : results['urls']['full'],
                             'body' : body,
                             'subject' : subject,
-                            'cid' :	'c_new5'
+                            'cid' : 'c_new5'
                         }
             response = rq.post('https://padlet.com/api/3/wishes', headers = headers, data = payload)
             if response.status_code == 201:
@@ -62,4 +72,6 @@ def main():
 
 for i in range(threads):
     x = threading.Thread(target = main)
-    x.start()
+    x.start() 
+
+
