0847ee5f1d874e59aabffb95a05898c659ed8ed4
Kodzima <76056308+Kodzima@users.noreply.github.com>
2021-10-24 13:29:31 +0000

    Update spamdlet.py

--- spamdlet.py
+++ spamdlet.py
@@ -20,7 +20,6 @@ print(f"""
 """)
 time.sleep(2)
 
-threads = 1
 
 search = input("Введите ваш запрос для фото: ").replace(" ", "+")
 
@@ -28,7 +27,11 @@ search = input("Введите ваш запрос для фото: ").replace("
 url = input("Введите ссылку на нужный вам падлет: ")
 subject = input('Введите заголовок для вложения (по желанию): ')
 body = input('Введите текст для вложения (по желанию): ')
-threads = int(input('Введите количество потоков для спамма (по умолчанию 1): '))
+threads = input('Введите количество потоков для спамма (по умолчанию 1): ')
+if threads != '':
+    threads = int(threads)
+else:
+    threads = 1
 
 response = rq.get(url)
 soup = bs4(response.content, 'lxml')
@@ -38,7 +41,7 @@ response = json.loads(response.content)
 wall_id = response['wall']['id']
 response = rq.get('https://padlet.com/api/5/wall_sections?wall_id=' + str(wall_id))
 response = json.loads(response.content)
-wall_section_id = response['data'][0]['id']
+wall_section_ids = [item['id'] for item in response['data']]
 headers = {
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
     'authorization' : 'Bearer 63002bc7cc05f2a3157ad7945e958e22cde27e81358c16ec53c7082b151a8010'
@@ -51,20 +54,21 @@ def main():
         response = rq.get(f"https://unsplash.com/napi/search/photos?query={search}&per_page=20&page={i}").text
         img = json.loads(response)
         for results in img['results']:
-            payload = {
-                            'wall_id' : wall_id,
-                            'wall_section_id' : wall_section_id,
-                            'attachment' : results['urls']['full'],
-                            'body' : body,
-                            'subject' : subject,
-                            'cid' : 'c_new5'
-                        }
-            response = rq.post('https://padlet.com/api/3/wishes', headers = headers, data = payload)
-            if response.status_code == 201:
-                print('Отправлено успешно')
-            else:
-                print(response.status_code)
-                print('Что-то пошло не так...')
+            for wall_section_id in wall_section_ids:
+                payload = {
+                                'wall_id' : wall_id,
+                                'wall_section_id' : wall_section_id,
+                                'attachment' : results['urls']['full'],
+                                'body' : body,
+                                'subject' : subject,
+                                'cid' : 'c_new5'
+                            }
+                response = rq.post('https://padlet.com/api/3/wishes', headers = headers, data = payload)
+                if response.status_code == 201:
+                    print('Отправлено успешно')
+                else:
+                    print(response.status_code)
+                    print('Что-то пошло не так...')
 
 
             
@@ -73,5 +77,3 @@ def main():
 for i in range(threads):
     x = threading.Thread(target = main)
     x.start() 
-
-
